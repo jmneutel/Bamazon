352e5f31e3308bc1da9a2585d7adf0e7a0794b18
Jared Neutel <Jared@Macintosh-2.local>
2017-04-21 08:52:23 -0700

    customer done

---
+++ bamazonCustomer.js
@@ -0,0 +1,86 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "Bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+
+    console.log("Here is a list of all the items for sale:");
+    for (var i = 0; i < results.length; i++) {
+
+    	console.log("----------------------------------");
+    	console.log("Item " + results[i].item_id + ": " + results[i].product_name);
+    	console.log("Price: $" + results[i].price);
+
+    }
+
+});
+
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+
+    inquirer.prompt([
+      {
+        name: "choice",
+        type: "rawlist",
+        choices: function() {
+          var choiceArray = [];
+          for (var i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].product_name);
+          }
+          return choiceArray;
+        },
+        message: "What item would you like to purchase?"
+      },
+      {
+        name: "purchase",
+        type: "input",
+        message: "How many units of this item would you like to buy?"
+      }
+    ]).then(function(answer) {
+      // get the information of the chosen item
+      var chosenItem;
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].product_name === answer.choice) {
+          chosenItem = results[i];
+        }
+      }
+      // console.log(chosenItem);
+
+      if (chosenItem.stock_quantity > parseInt(answer.purchase)) {
+
+      	var newQuantity = chosenItem.stock_quantity - answer.purchase;
+      	// console.log(newQuantity);
+
+        connection.query('UPDATE products SET ? WHERE ?',[{
+          stock_quantity: newQuantity
+        }, {
+          item_id: chosenItem.item_id
+        }], function(error) {
+          if (error) throw err;
+          console.log("Order successful!");
+          console.log("Total cost: " + (answer.purchase * chosenItem.price));
+        })
+      } else {
+
+        console.log("Insufficient quantity. Try again...");
+
+      };
+    });
+  });
